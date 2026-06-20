81042189fff37b5d87a06f09c6042511827b8f2e
Fco Javier Felix <ffelix@inode64.com>
2026-06-20 09:00:17 +0200

    refactor: rename inspect.go to helpers.go and move runDaemon function to daemon.go

--- internal/cli/helpers.go
+++ internal/cli/helpers.go
@@ -1,26 +1,11 @@
 package cli
 
 import (
-	"context"
 	"fmt"
 	"sort"
 
 	"sermo/internal/config"
 )
-
-// runDaemon dispatches sermod control subcommands.
-func (a App) runDaemon(ctx context.Context, opts options) int {
-	if len(opts.args) == 0 {
-		return a.commandUsageError("daemon", "daemon requires subcommand reload")
-	}
-	if opts.args[0] == "reload" {
-		if len(opts.args) > 1 {
-			return a.commandUsageError("daemon", "daemon reload takes no arguments")
-		}
-		return a.runReload(ctx, opts)
-	}
-	return a.commandUsageError("daemon", fmt.Sprintf("unknown daemon subcommand %q", opts.args[0]))
-}
 
 // requireService reports an unknown-service error unless name is configured.
 // It returns exitSuccess when the service exists.
