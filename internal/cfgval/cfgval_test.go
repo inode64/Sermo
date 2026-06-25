ce17d61301a8b5a13af67157a51e9b38d4e9723a
INODE64 <ffelix@inode64.com>
2026-06-25 11:13:30 +0200

    agent: test(cfgval): kill TB byte-size mutant

--- internal/cfgval/cfgval_test.go
+++ internal/cfgval/cfgval_test.go
@@ -175,6 +175,7 @@ func TestByteSize(t *testing.T) {
 		{"1.5G", 1536 << 20, true},
 		{"1GB", 1 << 30, true},
 		{"2T", 2 << 40, true},
+		{"1TB", 1 << 40, true},
 		{"1TiB", 1 << 40, true}, // TiB suffix (mutant .107)
 		{"1GiB", 1 << 30, true}, // GiB suffix (mutant .113)
 		{"0", 0, false},
